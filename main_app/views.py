c9565691c25ddb9ce110e23a63790de5ae5abadd
Ashley Sands <ashley.sands01@gmail.com>
2024-08-20 15:55:54 -0400

    update cat detail to match lesson notes

--- main_app/views.py
+++ main_app/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import Cat, Toy, Feeding
+from .models import Cat, Toy
 from .forms import FeedingForm
 
 # Create your views here.
@@ -23,39 +23,12 @@ def cat_index(request):
   cats = Cat.objects.filter(user=request.user)
   return render(request, 'cats/index.html', { 'cats': cats })
 
-
-class CatDetail(LoginRequiredMixin,DetailView):
-    model = Cat
-    template_name = 'cats/detail.html'
-    
-    # redirect user to home page if trying to view a cat they didn't make
-    def dispatch(self, request, *args, **kwargs):
-        self.object = self.get_object()
-
-        if self.object.user != self.request.user:
-            return redirect('home')
-        
-        return super().dispatch(request, *args, **kwargs)
-    
-    # pass in feeding form and toys
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['feeding_form'] = FeedingForm()
-        cat = Cat.objects.get(id=self.get_object().pk)
-        context['toys'] = Toy.objects.exclude(id__in = cat.toys.all().values_list('id'))
-        print(f"context {self.get_object().pk}")
-        return context
-
-    # posting the feeding form
-    def post(self, request, *args, **kwargs):
-        cat = self.get_object()  
-        form = FeedingForm(request.POST) 
-
-        if form.is_valid():
-            meal_text = form.cleaned_data['meal']
-            date_text = form.cleaned_data['date']
-            Feeding.objects.create(cat=cat, meal=meal_text, date=date_text)
-        return redirect('cat-detail', pk=cat.pk)
+@login_required
+def cat_detail(request, cat_id):
+  cat = Cat.objects.get(id=cat_id)
+  toys_cat_doesnt_have = Toy.objects.exclude(id__in = cat.toys.all().values_list('id'))
+  feeding_form = FeedingForm()
+  return render(request, 'cats/detail.html', {'cat': cat, 'feeding_form': feeding_form, 'toys': toys_cat_doesnt_have })
 
 class CatCreate(LoginRequiredMixin, CreateView):
   model = Cat
@@ -72,6 +45,16 @@ class CatUpdate(LoginRequiredMixin, UpdateView):
 class CatDelete(LoginRequiredMixin, DeleteView):
   model = Cat
   success_url = '/cats/'
+
+@login_required
+def add_feeding(request, cat_id):
+  form = FeedingForm(request.POST)
+
+  if form.is_valid():
+    new_feeding = form.save(commit=False)
+    new_feeding.cat_id = cat_id
+    new_feeding.save()
+  return redirect('cat-detail', cat_id=cat_id)
 
 class ToyCreate(LoginRequiredMixin, CreateView):
   model = Toy
@@ -95,14 +78,14 @@ class ToyDelete(LoginRequiredMixin, DeleteView):
 @login_required
 def associate_toy(request, cat_id, toy_id):
   Cat.objects.get(id=cat_id).toys.add(toy_id)
-  return redirect('cat-detail', pk=cat_id)
+  return redirect('cat-detail', cat_id=cat_id)
 
 @login_required
 def remove_toy(request, cat_id, toy_id):
   cat = Cat.objects.get(id=cat_id)
   toy = Toy.objects.get(id=toy_id)
   cat.toys.remove(toy)
-  return redirect('cat-detail', pk=cat.id)
+  return redirect('cat-detail', cat_id=cat.id)
 
 def signup(request):
     error_message = ''
