78dc9f5976d3f8007c64c02ce913831f74d4acd0
Ashley Sands <ashley.sands01@gmail.com>
2024-08-23 11:06:24 -0400

    remove password based auth section from signup form

--- main_app/views.py
+++ main_app/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Cat, Toy
-from .forms import FeedingForm
+from .forms import FeedingForm, RegisterForm
 
 # Create your views here.
 
@@ -82,17 +82,20 @@ def associate_toy(request, cat_id, toy_id):
 
 @login_required
 def remove_toy(request, cat_id, toy_id):
-  cat = Cat.objects.get(id=cat_id)
-  toy = Toy.objects.get(id=toy_id)
-  cat.toys.remove(toy)
-  return redirect('cat-detail', cat_id=cat.id)
+  # cat = Cat.objects.get(id=cat_id)
+  # toy = Toy.objects.get(id=toy_id)
+  # cat.toys.remove(toy)
+
+  Cat.objects.get(id=cat_id).toys.remove(toy_id)
+  return redirect('cat-detail', cat_id=cat_id)
+  # return redirect('cat-detail', cat_id=cat.id) #alternate way if using above commented out code
 
 def signup(request):
     error_message = ''
     if request.method == 'POST':
         # This is how to create a 'user' form object
         # that includes the data from the browser
-        form = UserCreationForm(request.POST)
+        form = RegisterForm(request.POST)
         if form.is_valid():
             # This will add the user to the database
             user = form.save()
@@ -102,7 +105,7 @@ def signup(request):
         else:
             error_message = 'Invalid sign up - try again'
     # A bad POST or a GET request, so render signup.html with an empty form
-    form = UserCreationForm()
+    form = RegisterForm()
     context = {'form': form, 'error_message': error_message}
     return render(request, 'signup.html', context)
     # Same as: 
