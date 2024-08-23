5224082b7b0cec359005e82a8a8ee19a8fdfa086
Ashley Sands <ashley.sands01@gmail.com>
2024-08-23 11:07:38 -0400

    remove password based auth section from signup form

--- main_app/views.py
+++ main_app/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Cat, Toy, Feeding
-from .forms import FeedingForm
+from .forms import FeedingForm, RegisterForm
 from django.contrib.auth.models import AnonymousUser
 
 # Create your views here.
@@ -121,7 +121,7 @@ def signup(request):
     if request.method == 'POST':
         # This is how to create a 'user' form object
         # that includes the data from the browser
-        form = UserCreationForm(request.POST)
+        form = RegisterForm(request.POST)
         if form.is_valid():
             # This will add the user to the database
             user = form.save()
@@ -131,7 +131,7 @@ def signup(request):
         else:
             error_message = 'Invalid sign up - try again'
     # A bad POST or a GET request, so render signup.html with an empty form
-    form = UserCreationForm()
+    form = RegisterForm()
     context = {'form': form, 'error_message': error_message}
     return render(request, 'signup.html', context)
     # Same as: 
