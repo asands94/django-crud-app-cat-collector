3379c21237f7573002a12e375e617b3e71ac820f
Ashley Sands <ashley.sands01@gmail.com>
2024-08-27 09:54:15 -0400

    add import

--- catcollector/settings.py
+++ catcollector/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from dotenv import load_dotenv
+import dj_database_url
 import os
 
 load_dotenv()
@@ -135,7 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / "my_app/staticfiles"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 LOGIN_URL = 'home'
 
